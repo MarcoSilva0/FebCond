c9193a00f9113d18b542636f1b26c32b853f9196
MarcoSilva0 <marcoantonio02016@outlook.com>
2022-09-12 23:48:04 -0300

    Criação dos controller e inserção na Routas da API
    Encerramento da configuração base
    
    Signed-off-by: MarcoSilva0 <marcoantonio02016@outlook.com>

--- ards-backend/routes/api.php
+++ ards-backend/routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BilletController;
+use App\Http\Controllers\DocController;
+use App\Http\Controllers\FoundAndLostController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\UnitController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\WallController;
+use App\Http\Controllers\WarningController;
+
 Route::get('/ping', function(){
     return['pong'=>true];
 });
