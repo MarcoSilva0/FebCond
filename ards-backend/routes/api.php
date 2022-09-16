f9c0126155d2822c4d91bf869ee8b04786ff3b83
MarcoSilva0 <marcoantonio02016@outlook.com>
2022-09-15 23:06:59 -0300

    Configurado algumas rotas do sistema. Rotas essencias para o aplicativo
    Signed-off-by: MarcoSilva0 <marcoantonio02016@outlook.com>

--- ards-backend/routes/api.php
+++ ards-backend/routes/api.php
@@ -16,3 +16,25 @@ use App\Http\Controllers\WarningController;
 Route::get('/ping', function(){
     return['pong'=>true];
 });
+
+Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function(){
+    Route::post('/auth/validate', [AuthController::class, 'validateToken']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+    // Mural de avisos
+    Route::get('/walls', [WallController::class, 'getAll']);
+    Route::post('/wall/{id}/like', [WallController::class, 'like']);
+
+    // Documentos
+    Route::get('/docs', [DocController::class, 'getAll']);
+
+    // Livro de ocorrência
+    Route::get('/warnings', [WarningController::class, 'getMyWarnings']);
+    Route::post('/warning', [WarningController::class, 'setWarning']);
+    Route::post('/warning/file', [WarningController::class, 'addWarningFile']);
+});
