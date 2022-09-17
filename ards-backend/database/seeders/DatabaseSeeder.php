d2747b310ae31736eb7f472c00aee1af28455376
MarcoSilva0 <marcoantonio02016@outlook.com>
2022-09-17 19:16:20 -0300

    Criado Seeder para popular o banco. Finalidade de teste
    Signed-off-by: MarcoSilva0 <marcoantonio02016@outlook.com>

--- ards-backend/database/seeders/DatabaseSeeder.php
+++ ards-backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
+use App\Models\User;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,6 +16,52 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::factory(10)->create();
+        DB::table('units')->insert([
+            'name' => 'APT 100',
+            'id_owner' => '1'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 101',
+            'id_owner' => '1'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 200',
+            'id_owner' => '0'
+        ]);
+        DB::table('units')->insert([
+            'name' => 'APT 201',
+            'id_owner' => '0'
+        ]);
+
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Academia',
+            'cover' =>  'gym.jpg',
+            'days' => '1,2,4,5',
+            'start_time' => '06:00:00',
+            'end_time' => '23:00:00'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Piscina',
+            'cover' =>  'pool.jpg',
+            'days' => '1,2,3,4,5',
+            'start_time' => '07:00:00',
+            'end_time' => '22:00:00'
+        ]);
+        DB::table('areas')->insert([
+            'allowed' => '1',
+            'title' => 'Churrasqueira',
+            'cover' =>  'barbecue.jpg',
+            'days' => '4,5,6',
+            'start_time' => '09:00:00',
+            'end_time' => '23:00:00'
+        ]);
+
+        DB::table('walls')->insert([
+            'title' => 'Título de Aviso de Teste',
+            'body' => 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.',
+            'datecreated' => '2022-09-17 19:12'
+        ]);
     }
 }
