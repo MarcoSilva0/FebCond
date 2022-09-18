81db5086a6aac3d626410bc230ba3d0c0c1cd464
MarcoSilva0 <marcoantonio02016@outlook.com>
2022-09-18 11:11:19 -0300

    Configuração de rotas do mural de aviso
    Signed-off-by: MarcoSilva0 <marcoantonio02016@outlook.com>

--- ards-backend/app/Http/Controllers/WallController.php
+++ ards-backend/app/Http/Controllers/WallController.php
@@ -3,8 +3,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Wall;
+use App\Models\WallLike;
 
 class WallController extends Controller
 {
-    //
+    public function getAll(){
+        $arr = ['error' => '', 'list' => []];
+
+        //Usuário logado
+        $user = auth()->user();
+
+        $walls = Wall::all();
+
+        foreach($walls as $wallkey => $wallValue){
+            $walls[$wallkey]['likes'] = 0;
+            $walls[$wallkey]['liked'] = false;
+
+            $likes = WallLike::where('id_wall', $wallValue['id'])->count();
+            $walls[$wallkey]['likes'] = $likes;
+
+            $melikes = WallLike::where('id_wall', $wallValue['id'])
+            ->where('id_user', $user['id'])
+            ->count();
+
+            if($melikes > 0){
+                $walls[$wallkey]['liked'] = true;
+            }
+        }
+
+        $arr['list'] = $walls;
+
+        return $arr;
+    }
+
+    public function like($id){
+        $arr = ['error' => ''];
+
+        //Usuário logado
+        $user = auth()->user();
+
+        $melikes = WallLike::where('id_wall', $id)
+        ->where('id_user', $user['id'])
+        ->count();
+
+        if($melikes > 0){
+            // Remove like
+            WallLike::where('id_wall', $id)
+            ->where('id_user', $user['id'])
+            ->delete();
+
+            $arr['liked'] = false;
+        }else{
+            // Adiciona like
+            $newLike = new WallLike();
+            $newLike->id_wall = $id;
+            $newLike->id_user = $user['id'];
+            $newLike->save();
+
+            $arr['liked'] = true;
+        }
+
+        $arr['likes'] = WallLike::where('id_wall', $id)->count();
+
+        return $arr;
+    }
 }
